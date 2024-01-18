1556cd4fba24d6a41d2bb1408fed311dba83887b
Gristyle <156291797+Gristyle@users.noreply.github.com>
2024-01-17 21:40:05 -0600

    Update index.js
    Just playing around only added info under "Example" comment

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+//Example
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const bodyParser = require("body-parser");
+
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -21,4 +30,60 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+//Example
+
+let urlSchema = new mongoose.Schema({
+  original: {
+    type: String,
+    required: true},
+  short: {
+    type: Number}
+});
+
+let middleware = app.use(bodyParser.urlencoded({extended: false})); 
+
+let Url = mongoose.model("Url", urlSchema);
+
+app.post("/api/shorturl", middleware, (req, res) => {
+  let inputUrl = req.body.url;
+  {original_url: inputUrl};
+
+  if (! /^https?:\/\/.*[.].+/.test(req.body.url)) {
+    res.json({
+      error: "invalid url"
+    });
+  } else {
+      const urlObject = {
+        "original_url": req.body.url,
+        "short_url": urlCount
+      }
+    };
+
+    urlList.push(urlObject);
+    urlCount++;
+    res.json(urlObject);
+    
+  let inputShortUrl = 1;
+
+  Url.findOne({})
+    .sort({short: "desc"})
+    .exec((err, searchData) => {
+      if(!err && searchData != undefined) {
+        inputShortUrl = searchData.short + 1
+      };
+      if(!err){
+        Url.findOneAndUpdate(
+          {original: inputUrl},
+          {original: inputUrl, short: inputShortUrl},
+          {new: true, upsert: true},
+          (err, data) => {
+            if(!err){
+              res.json({short_url: data.short})
+            };
+          }
+        )
+      };
+    })
 });
